4f1d4b352fd8bb579bb8ff1b79eb2d40f154503b
ight <thelight.prakash247@gmail.com>
2015-11-06 16:42:31 +0530

    NGO: Intial Commit and Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NGO::Application.config.secret_token = '96d5c777d3026fd98ea5168203b6ff894c99d42acfc29b350c7328c17d33d79c09daa16c22065ed934de12d5f79768ff16fa98107faf1ae0fb3482468cc74b15'
